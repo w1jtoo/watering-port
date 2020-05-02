d9ff7ae10e62921c03f1283077a8969c6668dd2f
Kharlov Vasiliy <kharlov.v@skbkontur.ru>
2020-05-02 20:42:40 +0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,45 @@
+use async_std::future::timeout;
 use async_std::net::TcpStream;
 use futures::executor::block_on;
 use futures::future::join_all;
-use futures::join;
 use std::io;
+use std::time::Duration;
 
-
-fn main() -> io::Result<()>  {
-    block_on(chech_ports());
+fn main() -> io::Result<()> {
+    let ports = block_on(get_ports(Duration::from_secs(2)));
+    println!("{:?}", ports);
     Ok(())
 }
 
-async fn chech_ports() {
-    // Join the two futures together
+async fn get_ports(duration: Duration) -> Vec<u16> {
     let mut tasks = Vec::new();
 
-    for port in 1..500 { 
-        tasks.push(check_port(port as u16))
+    for port in 0..500 {
+        tasks.push(check_port(port, duration));
     }
 
-    join!(join_all(tasks));
+    let port_results = join_all(tasks).await;
+
+    port_results
+        .into_iter()
+        .filter(|x| x.is_some())
+        .map(|p| p.unwrap())
+        .collect()
 }
 
+async fn check_port(port: u16, duration: Duration) -> Option<u16> {
+    println!("{}", port);
 
-async fn check_port(port: u16){
-    match TcpStream::connect(format!("13.90.224.253:{}", port)).await { 
-        Ok(_) => {
-            println!("PORT {} OPENED", port); 
+    match timeout(
+        duration,
+        TcpStream::connect(format!("13.90.224.253:{}", port)),
+    )
+    .await
+    {
+        Ok(f) => match f {
+            Ok(_) => Some(port),
+            Err(_) => None,
         },
-        Err(_) => println!("PORT {} CLOSED", port),
-    } 
+        Err(_) => None,
+    }
 }
